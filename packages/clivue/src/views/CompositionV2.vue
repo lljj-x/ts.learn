02efb68f368d53e277457bfb98288f4a59d23234
Liu Jun <870395192@qq.com>
2020-12-25 11:20:25 +0800

    chore: 啥也不是

---
+++ packages/clivue/src/views/CompositionV2.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="box">
+        {{ date }}
+        <p>
+            <button @click="onUpdate">更新时间</button>
+        </p>
+    </div>
+</template>
+
+<script>
+// 啥也不是
+export default {
+    name: 'CompositionV2',
+    data() {
+        // methods
+        this.onUpdate = () => {
+            this.date = new Date();
+        };
+
+        // Life cycle
+        this.$on('hook:created', () => {
+            console.log('hook:created');
+        });
+
+        // watch
+        setTimeout(() => {
+            this.$watch('date', () => {
+                debugger;
+            });
+        });
+
+        // reactive
+        return {
+            date: new Date()
+        };
+    }
+};
+</script>
+
+<style scoped>
+.box {
+    padding: 40px 10px;
+    background: #efefef;
+}
+</style>
--- packages/clivue/src/views/Home.vue
+++ packages/clivue/src/views/Home.vue
@@ -5,17 +5,20 @@
             src="../assets/logo.png"
         >
         <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+        <CompositionV2></CompositionV2>
     </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue';
+import CompositionV2 from './CompositionV2.vue';
 
 // @ is an alias to /src
 @Component({
     components: {
         HelloWorld,
+        CompositionV2
     },
 })
 export default class Home extends Vue {}
